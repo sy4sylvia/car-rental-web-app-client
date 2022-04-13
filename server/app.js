a51cd0d63d7f2458038c16e04e03fa0a13aad63b
Siya Guo <sguo713@gmail.com>
2022-04-12 21:39:09 -0400

    mysql

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mysql = require("mysql2");
 
 //bcrypt
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const session = require("express-session");
 
 const app = express();
 
@@ -13,15 +13,40 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-//initialize session -> this is for cookies, might not be necessary
-app.use(session({
-    secret: "any long string could be the secret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {}
-}));
+
 
 //database setup
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'WOW'
+});
+
+connection.connect(function (err){
+    if (err) console.log(err);
+});
+
+// simple query
+connection.query('SELECT * FROM `sjd_order` WHERE `end_odometer` > 1000', (err, results, fields) => {
+    if (err) console.log(err);
+    else {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    }
+})
+
+// with placeholder
+// connection.query(
+//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//     ['Page', 45],
+//     function(err, results) {
+//         if (err) console.log(err);
+//         else console.log(results);
+//     }
+// );
+
+
 
 //CRUD
 // app.route("/")
@@ -36,4 +61,6 @@ app.use(session({
 
 app.listen(3000, function() {
     console.log("Server started on localhost 3000");
-})
+});
+
+connection.end();
