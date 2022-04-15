80fbf175dce83e398ffa8d823ebe20ee32a4beab
Siya Guo <sguo713@gmail.com>
2022-04-15 14:50:03 -0400

    handleChange register

--- wow-client/src/components/register.js
+++ wow-client/src/components/register.js
@@ -7,14 +7,31 @@ function Register(){
 
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
-    const [driverLicenseNo, setdriverLicenseNo] = useState("");
+    const [driverLicenseNo, setDriverLicenseNo] = useState("");
 
     function handleClick(event) {
 
     }
 
-    function handleChange(event) {
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value);
+    }
+    function handleMiddleNameChange(event) {
+        setMiddleName(event.target.value);
+    }
+    function handleLastNameChange(event) {
+        setLastName(event.target.value);
+    }
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
 
+    function handlePhoneChange(event) {
+        setPhone(event.target.value);
+    }
+
+    function handleDriverLicenseNoChange(event) {
+        setDriverLicenseNo(event.target.value);
     }
 
     return (
@@ -24,13 +41,13 @@ function Register(){
             {/* form would refresh the page */}
             <form onSubmit={handleClick} name = "information">
                 {/*need to add action*/}
-                <input onChange = {handleChange} type="text" placeholder="First Name" value={firstName} />
-                <input onChange = {handleChange} type="text" placeholder="Middle Name(optional)" value={middleName} />
-                <input onChange = {handleChange} type="text" placeholder="Last Name" value={lastName} />
+                <input onChange = {handleFirstNameChange} type="text" placeholder="First Name" value={firstName} />
+                <input onChange = {handleMiddleNameChange} type="text" placeholder="Middle Name(optional)" value={middleName} />
+                <input onChange = {handleLastNameChange} type="text" placeholder="Last Name" value={lastName} />
 
-                <input onChange = {handleChange} type="text" placeholder="Email" value={email} />
-                <input onChange = {handleChange} type="text" placeholder="Mobile Phone" value={phone} />
-                <input onChange = {handleChange} type="text" placeholder="Driver License Number" value={driverLicenseNo} />
+                <input onChange = {handleEmailChange} type="text" placeholder="Email" value={email} />
+                <input onChange = {handlePhoneChange} type="text" placeholder="Mobile Phone" value={phone} />
+                <input onChange = {handleDriverLicenseNoChange} type="text" placeholder="Driver License Number" value={driverLicenseNo} />
                 <button onClick={handleClick}>Submit</button>
             </form>
 
