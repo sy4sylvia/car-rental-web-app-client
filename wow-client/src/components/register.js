3355d9b6cc4783d6ce867da52377d1ff4cb14427
Siya Guo <sguo713@gmail.com>
2022-04-15 15:18:23 -0400

    simplified

--- wow-client/src/components/register.js
+++ wow-client/src/components/register.js
@@ -1,38 +1,94 @@
 import React, {useState} from 'react';
 
 function Register(){
-    const [firstName, setFirstName] = useState("");
-    const [middleName, setMiddleName] = useState("");
-    const [lastName, setLastName] = useState("");
+    // const [firstName, setFirstName] = useState("");
+    // const [middleName, setMiddleName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    //
+    // const [email, setEmail] = useState("");
+    // const [phone, setPhone] = useState("");
+    // const [driverLicenseNo, setDriverLicenseNo] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [driverLicenseNo, setDriverLicenseNo] = useState("");
+    const [information, setInformation] = useState({
+        firstName: "",
+        middleName:"",
+        email:"",
+        phone:"",
+        driverLicenseNo:""
+    });
+
+    const [address, setAddress] = useState({
+        street: "",
+        apt:"",
+        city:"",
+        zipcode:"",
+        state:"",
+        country:""
+    });
+
+    const handleInformationChange = (event) => {
+        setInformation((prevalue) => {
+            return {
+                ...prevalue,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    const handleAddressChange = (event) => {
+        setAddress((prevalue) => {
+            return {
+                ...prevalue,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
 
     function handleClick(event) {
 
     }
 
-    function handleFirstNameChange(event) {
-        setFirstName(event.target.value);
-    }
-    function handleMiddleNameChange(event) {
-        setMiddleName(event.target.value);
-    }
-    function handleLastNameChange(event) {
-        setLastName(event.target.value);
-    }
-    function handleEmailChange(event) {
-        setEmail(event.target.value);
-    }
 
-    function handlePhoneChange(event) {
-        setPhone(event.target.value);
-    }
 
-    function handleDriverLicenseNoChange(event) {
-        setDriverLicenseNo(event.target.value);
-    }
+    // function handleFirstNameChange(event) {
+    //     setFirstName(event.target.value);
+    // }
+    // function handleMiddleNameChange(event) {
+    //     setMiddleName(event.target.value);
+    // }
+    // function handleLastNameChange(event) {
+    //     setLastName(event.target.value);
+    // }
+    // function handleEmailChange(event) {
+    //     setEmail(event.target.value);
+    // }
+    //
+    // function handlePhoneChange(event) {
+    //     setPhone(event.target.value);
+    // }
+    //
+    // function handleDriverLicenseNoChange(event) {
+    //     setDriverLicenseNo(event.target.value);
+    // }
+    //
+    // function handleStreetChange(event) {
+    //     setEmail(event.target.value);
+    // }
+    //
+    // function handleAptChange(event) {
+    //     setPhone(event.target.value);
+    // }
+    //
+    // function handleCityChange(event) {
+    //     setDriverLicenseNo(event.target.value);
+    // }
+    // function handleZipcodeChange(event) {
+    //     setPhone(event.target.value);
+    // }
+    //
+    // function handleCityChange(event) {
+    //     setDriverLicenseNo(event.target.value);
+    // }
+
 
     return (
         <div className="container">
@@ -41,13 +97,13 @@ function Register(){
             {/* form would refresh the page */}
             <form onSubmit={handleClick} name = "information">
                 {/*need to add action*/}
-                <input onChange = {handleFirstNameChange} type="text" placeholder="First Name" value={firstName} />
-                <input onChange = {handleMiddleNameChange} type="text" placeholder="Middle Name(optional)" value={middleName} />
-                <input onChange = {handleLastNameChange} type="text" placeholder="Last Name" value={lastName} />
+                <input onChange = {handleInformationChange} type="text" placeholder="First Name" value={information.firstName} />
+                <input onChange = {handleInformationChange} type="text" placeholder="Middle Name(optional)" value={information.middleName} />
+                <input onChange = {handleInformationChange} type="text" placeholder="Last Name" value={information.lastName} />
 
-                <input onChange = {handleEmailChange} type="text" placeholder="Email" value={email} />
-                <input onChange = {handlePhoneChange} type="text" placeholder="Mobile Phone" value={phone} />
-                <input onChange = {handleDriverLicenseNoChange} type="text" placeholder="Driver License Number" value={driverLicenseNo} />
+                <input onChange = {handleInformationChange} type="text" placeholder="Email" value={information.email} />
+                <input onChange = {handleInformationChange} type="text" placeholder="Mobile Phone" value={information.phone} />
+                <input onChange = {handleInformationChange} type="text" placeholder="Driver License Number" value={information.driverLicenseNo} />
                 <button onClick={handleClick}>Submit</button>
             </form>
 
@@ -55,12 +111,12 @@ function Register(){
             <p>Please edit your address here.</p>
             <form onSubmit={handleClick} name = "address">
                 {/*need to add action*/}
-                <input onChange = {handleChange} type="text" placeholder="Street" value={email} />
-                <input onChange = {handleChange} type="text" placeholder="Apt(optional)" value={phone} />
-                <input onChange = {handleChange} type="text" placeholder="City" value={lastName} />
-                <input onChange = {handleChange} type="text" placeholder="Zipcode" value={email} />
-                <input onChange = {handleChange} type="text" placeholder="State" value={phone} />
-                <input onChange = {handleChange} type="text" placeholder="Country" value={lastName} />
+                <input onChange = {handleAddressChange} type="text" placeholder="Street" value={address.street} />
+                <input onChange = {handleAddressChange} type="text" placeholder="Apt(optional)" value={address.apt} />
+                <input onChange = {handleAddressChange} type="text" placeholder="City" value={address.city} />
+                <input onChange = {handleAddressChange} type="text" placeholder="Zipcode" value={address.zipcode} />
+                <input onChange = {handleAddressChange} type="text" placeholder="State" value={address.state} />
+                <input onChange = {handleAddressChange} type="text" placeholder="Country" value={address.country} />
 
                 <button onClick={handleClick}>Submit</button>
             </form>
