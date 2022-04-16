77dfac1bc2bc371788829b3b5d8a8a6f61f36e75
Siya Guo <sguo713@gmail.com>
2022-04-16 17:35:05 -0400

    rendering components

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,20 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React from "react";
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
-
-import './App.css';
-import React from 'react';
-import {Link} from "react-router-dom";
-// import { BrowserRouter, Link, Route, Routes} from "react-router-dom";
-
-//import all the components we want to render in our page.
-import Home from "./routes/Home";
-import Login from './routes/Login';
-import Register from "./routes/Register";
-import Test from "./routes/Test";
-import Navigation from "./components/Navigation";
-
-// function App() {
-//     return (
-//         <div className="App">
-//             {/*<Navigation></Navigation>*/}
-//             <BrowserRouter>
-//                 <Routes>
-//                     <Route exact path = '/test' element = {<Test />} />
-//                     <Route exact path = '/login' element = {<Login />} />
-//                     <Route exact path = '/register' element = {<Register />} />
-//                     <Route exact path = '/' element = {<Home />} />
-//                     {/*default route comes last*/}
-//                 </Routes>
-//             </BrowserRouter>
-//         </div>
-//     );
-// }
-
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
     return (
         <div>
-            <h1>omg</h1>
+            <nav
+                style={{
+                    borderBottom: "solid 1px",
+                    paddingBottom: "1rem",
+                }}
+            >
+                <Link to="/register">Register</Link> |{" "}
+                <Link to="/login">Log in</Link>
+            </nav>
+            <Outlet />
         </div>
     );
 }
 
-
-// const App = () => (
-//     <Router>
-//         <Routes>
-//           <Route path = '/login' element = {<Login />} />
-//           <Route path = '/register' element = {<Register />} />
-//           <Route path = '/' element = {<Home />} />
-//             {/*default route comes last*/}
-//         </Routes>
-//     </Router>
-// );
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,21 @@
 import { render } from "react-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 import App from "./App";
 import './index.css';
+import Register from "./routes/Register";
+import Login from "./routes/Login";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(<BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route path="register" element={<Register />} />
+                <Route path="login" element={<Login />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>,
+     rootElement);
